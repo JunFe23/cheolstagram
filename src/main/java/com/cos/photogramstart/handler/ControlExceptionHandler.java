b8f220355a4977b6fa9b137ab787728009717f93
JunFe23 <ogum0405@hanmail.net>
2021-06-08 23:45:05 +0900

    2021.06.08

--- src/main/java/com/cos/photogramstart/handler/ControlExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControlExceptionHandler.java
@@ -7,14 +7,25 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.photogramstart.handler.ex.CustomValidationException;
+import com.cos.photogramstart.util.Script;
 import com.cos.photogramstart.web.dto.CMRespDto;
 
 @RestController
 @ControllerAdvice // exception이 발생할 때 낚아챔.
 public class ControlExceptionHandler {
 
+//	@ExceptionHandler(CustomValidationException.class) // runtimeexception이 발생하는 모든 exception을 함수가 가로챈다.
+//	public CMRespDto<?> validationException(CustomValidationException e) {
+//		return new CMRespDto<Map<String, String>> (-1,e.getMessage(), e.getErrorMap());
+//	}
+	
 	@ExceptionHandler(CustomValidationException.class) // runtimeexception이 발생하는 모든 exception을 함수가 가로챈다.
-	public CMRespDto<?> validationException(CustomValidationException e) {
-		return new CMRespDto<Map<String, String>> (-1,e.getMessage(), e.getErrorMap());
+	public String validationException(CustomValidationException e) {
+		
+		// CMRespDto vs Script 비교
+		// 1. 클라이언트에게 응답할 때는 Script 좋음
+		// 2. Ajax 통신 - CMRespDto 좋음
+		// 3. Android 통신 - CMRespDto 좋음.
+		return Script.back(e.getErrorMap().toString());
 	}
 }
