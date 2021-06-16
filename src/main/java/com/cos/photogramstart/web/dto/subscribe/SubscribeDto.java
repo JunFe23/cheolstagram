e91d142c65e114bb029d84cfe51027d53f381b3f
JunFe23 <ogum0405@hanmail.net>
2021-06-16 13:30:42 +0900

    2021.06.16 구독정보 진행중.

---
+++ src/main/java/com/cos/photogramstart/web/dto/subscribe/SubscribeDto.java
@@ -0,0 +1,18 @@
+package com.cos.photogramstart.web.dto.subscribe;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class SubscribeDto {
+	private int id; // 구독한 사용자들의 id.
+	private String username; // 구독한 사용자의 유저네임.
+	private String profileImageUrl; // 사진.
+	private Integer subscribeState; // 구독한 상태인지.
+	private Integer equlUserState; // 로그인 한 동일 아이디인지.
+}
