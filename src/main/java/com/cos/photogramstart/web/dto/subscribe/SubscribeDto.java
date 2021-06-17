1d4f31ed426be7053169b1166b1a69ddb2b7db75
JunFe23 <ogum0405@hanmail.net>
2021-06-17 10:43:36 +0900

    2021.06.17 구독정보 뷰 렌더링 완료.

--- src/main/java/com/cos/photogramstart/web/dto/subscribe/SubscribeDto.java
+++ src/main/java/com/cos/photogramstart/web/dto/subscribe/SubscribeDto.java
@@ -14,5 +14,5 @@ public class SubscribeDto {
 	private String username; // 구독한 사용자의 유저네임.
 	private String profileImageUrl; // 사진.
 	private Integer subscribeState; // 구독한 상태인지.
-	private Integer equlUserState; // 로그인 한 동일 아이디인지.
+	private Integer equalUserState; // 로그인 한 동일 아이디인지.
 }
